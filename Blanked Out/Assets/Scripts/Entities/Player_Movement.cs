731ed18e12eee406eb9909a7723944d63e333c7f
KaizoVorna <KaizoVorna@gmail.com>
2025-04-17 12:26:34 +0300

    Added scripts that wel'll need
    Nothing ON the scripts yet, but they're there in Unity for anyone to pick up and start programming. (Will add a To-Do list to each, give me a moment.)

---
+++ Blanked Out/Assets/Scripts/Entities/Player_Movement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+    //This manages Frank's basic movement when unhindered and while carrying something.
+    //To Do:
+    //-Create a basic tile-based walking script.
+    //-Create a sneaking and running states, which go slower and faster respectively.
+    //-Create a jump script which jumps farther if player is in running state.
+    //-Create a mechanic for grabbing ledges incl. pulling up, letting go, descending from standing on a ledge.
+    //-Create a carrying state for when Frank is holding a bomb or a key.
+    //-Create a climbing state for getting up ropes.
+
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
