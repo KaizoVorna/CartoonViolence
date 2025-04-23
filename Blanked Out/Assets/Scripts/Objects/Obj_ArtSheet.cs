a624f95e86a9462d79e3a2f11a63e04b2c284520
KaizoVorna <KaizoVorna@gmail.com>
2025-04-23 13:07:17 +0300

    Did some compatibility
    Now we have Frank and an art sheet in there.

--- Blanked Out/Assets/Scripts/Objects/Obj_ArtSheet.cs
+++ Blanked Out/Assets/Scripts/Objects/Obj_ArtSheet.cs
@@ -6,7 +6,6 @@ public class Obj_ArtSheet : MonoBehaviour
     //This is the behaviour for the collectible art sheets.
     //To Do:
     //-Create a method to collect it. Walk over it? Turn to the background and press interact?
-    //-Create behaviour that increases your art sheet count and unlocks concept art in ObjectManager.
 
     public delegate void OnCollect(int value);
     public static event OnCollect onCollect;
