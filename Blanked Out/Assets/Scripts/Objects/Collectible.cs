4488d06fc652521d58fac3611d169f51c3ee2347
taskurotta1 <mustonenmedia@gmail.com>
2025-04-28 14:27:51 +0300

    Working with Collectible script
    I worked with collectible and collectible count scripts and managed to get them work at least somehow.

--- Blanked Out/Assets/Scripts/Objects/Collectible.cs
+++ Blanked Out/Assets/Scripts/Objects/Collectible.cs
@@ -1,13 +1,32 @@
+using System;
 using UnityEngine;
 
 public class Collectible: MonoBehaviour
 {
+    public static Action OnCollected; 
+    public static int total;
+   
 
-    void OnTriggerEnter(Collider other)
+    void Awake() => total++;
+      
+    
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Destroy(this.gameObject);
+            CollectibleCount count = other.GetComponent<CollectibleCount>();
+            {
+                if (count != null)
+                {
+                    count.OnCollectibleCollected();
+                }
+
+                OnCollected?.Invoke();
+                Destroy(gameObject);
+            }
+
+            
         }
     }
+   
 }
