6d15888116e5ece3980ece2a3333d88dd8134a3a
KaizoVorna <KaizoVorna@gmail.com>
2025-05-14 09:32:45 +0300

    Updated level design

--- Blanked Out/Assets/Scripts/Objects/Obj_Sign.cs
+++ Blanked Out/Assets/Scripts/Objects/Obj_Sign.cs
@@ -7,12 +7,6 @@ public class Obj_Sign : MonoBehaviour
     //-Make it a background interactable, which displays an image on the canvas and locks player movement.
     //-Make the image disappear and unlock movement when interact is pressed again.
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
