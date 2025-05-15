8fdaa3445b474bb64771dc002cd489d74f574646
KLIMenkoXL <taneliklimenko@gmail.com>
2025-05-15 12:17:05 +0300

    Added Follow script for DrawBad

---
+++ Blanked Out/Assets/Follow.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject Target;
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position , Target.transform.position , 10 * Time.deltaTime);
+    }
+}
