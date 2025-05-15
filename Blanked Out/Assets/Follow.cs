d9f6b3b261d54b8c66ad056da344b82cd02bca2f
KLIMenkoXL <taneliklimenko@gmail.com>
2025-05-15 14:41:12 +0300

    Changed DrawBat script (content)

--- Blanked Out/Assets/Follow.cs
+++ Blanked Out/Assets/Follow.cs
@@ -4,9 +4,43 @@ using UnityEngine;
 
 public class Follow : MonoBehaviour
 {
-    public GameObject Target;
+    private Vector2 startPosition; 
+    private bool returningToStart = true;
+
+    private void Start()
+    {
+        if (Input.GetKeyDown("DrawPad"))
+        {
+            startPosition = transform.position;
+        }
+    }
+    public Transform player;
+    public float followSpeed = 15f;
+    private bool isFollowing = true;
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position , Target.transform.position , 10 * Time.deltaTime);
+        if (isFollowing)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime);
+        }
+        if (isFollowing)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime);
+            }
+            else if (returningToStart)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, startPosition, followSpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, startPosition) < 10f)
+                {
+                    returningToStart = false;
+                }
+            }
+        }
+
     }
-}
+
+
+
+
+    
+    
--- Blanked Out/Assets/Scripts/Managers/DrawBadManager.cs
+++ Blanked Out/Assets/Scripts/Managers/DrawBadManager.cs
@@ -6,16 +6,26 @@ public class DrawBadManager : MonoBehaviour
 {
 
     public GameObject drawpbadMenuUI;
+    public float followSpeed = 15f;
+    public Transform player;
+    public Vector2 startPos;
 
+    void Start()
+    {
+        startPos = drawpbadMenuUI.transform.position;
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("DrawBad"))
         {
+
             drawpbadMenuUI.SetActive(true);
+            drawpbadMenuUI.transform.position = Vector2.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime);
         }
         else if (Input.GetButtonUp("DrawBad"))
         {
+            drawpbadMenuUI.transform.position = startPos;
             drawpbadMenuUI.SetActive(false);
         }
         
