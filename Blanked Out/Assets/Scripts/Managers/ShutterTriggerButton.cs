2ad989a9dd1b9397a13227c04d9fd5f00b8d3636
KaizoVorna <KaizoVorna@gmail.com>
2025-05-20 09:24:10 +0300

    Update ShutterTriggerButton.cs

--- Blanked Out/Assets/Scripts/Managers/ShutterTriggerButton.cs
+++ Blanked Out/Assets/Scripts/Managers/ShutterTriggerButton.cs
@@ -5,16 +5,29 @@ using UnityEngine.WSA;
 public class ShutterTriggerButton : MonoBehaviour
 {
     [SerializeField] private ShutterSetActive shutter;
+    bool canTrigger = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && canTrigger)
+        {
+            Debug.Log("Pushed");
+            Activate();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canTrigger = false;
+    }
 
     private void OnTriggerStay2D(Collider2D player)
     {
+        Debug.Log("Something detected");
         if (player.tag == "Player")
         {
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                Debug.Log("Pushed");
-                Activate();
-            }
+            Debug.Log("Player Detected");
+            canTrigger = true;
 
         }
     }
