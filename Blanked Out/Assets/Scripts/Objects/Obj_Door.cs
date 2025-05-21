66233936fcc0cdf290d296eee7860ae602cf564e
KaizoVorna <KaizoVorna@gmail.com>
2025-05-21 10:23:51 +0300

    Added doors
    ...DOORS, I tell ya

--- Blanked Out/Assets/Scripts/Objects/Obj_Door.cs
+++ Blanked Out/Assets/Scripts/Objects/Obj_Door.cs
@@ -6,9 +6,7 @@ public class Obj_Door : MonoBehaviour
     //To Do:
     //-Create an opening door script. If player manages to activate the script, they will become invincible for the duration.
     //-Create a midway save during the animation. (Loading screen allowed)
-    //-Maybe reset the previous area unless otherwise stated?
-    //-Or for ease, disallow going back through it.
-    //-Make it despawn all objects and entities from last area and load in the ones from the next.
+    //-Basically act as the boundary between scenes. Disallow going back through one.
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
