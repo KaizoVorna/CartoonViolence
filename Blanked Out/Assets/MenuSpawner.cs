157af57a1a0abd56fe48ca0729a727c5d9aa775d
KLIMenkoXL <taneliklimenko@gmail.com>
2025-05-27 12:44:39 +0300

    DB Menu script

--- Blanked Out/Assets/MenuSpawner.cs
+++ Blanked Out/Assets/MenuSpawner.cs
@@ -4,29 +4,85 @@ using UnityEngine;
 
 public class MenuSpawner : MonoBehaviour
 {
-        public GameObject prefabToSpawn;
+        public GameObject CampFire;
         public Transform spawnPoint;
         public int maxSpawnCount = 1;
         private int currentSpawnCount = 0;
+        public GameObject anvil;
+        public GameObject Bomb;
+        public GameObject Door;
 
         public void SpawnObject()
         {
-            if(currentSpawnCount >= maxSpawnCount)
-                {
-            Debug.Log("Spawn limit reached.");
-                return;
-                } 
 
-            Vector2 spawnPos = spawnPoint ? spawnPoint.position : Vector2.zero;
-            Instantiate(prefabToSpawn, spawnPos, Quaternion.identity);
-            currentSpawnCount++;
         }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetAxisRaw("Vertical") == -1f && anvil != null)
         {
-            SpawnObject();
+            if (currentSpawnCount >= maxSpawnCount)
+            {
+                Debug.Log("Spawn limit reached.");
+                return;
+            }
+
+            Vector2 spawnPos = spawnPoint ? spawnPoint.position : Vector2.zero;
+            Instantiate(anvil, spawnPos, Quaternion.identity);
+            currentSpawnCount++;
         }
+
+        if(Input.GetAxisRaw("Vertical") == 1f && Door != null)
+            {
+            if (currentSpawnCount >= maxSpawnCount)
+            {
+                Debug.Log("Spawn limit reached.");
+                return;
+            }
+
+            Vector2 spawnPos = spawnPoint ? spawnPoint.position : Vector2.zero;
+            Instantiate(Door, spawnPos, Quaternion.identity);
+            currentSpawnCount++;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == 1f && Bomb != null)
+        {
+            if (currentSpawnCount >= maxSpawnCount)
+            {
+                Debug.Log("Spawn limit reached.");
+                return;
+            }
+
+            Vector2 spawnPos = spawnPoint ? spawnPoint.position : Vector2.zero;
+            Instantiate(Bomb, spawnPos, Quaternion.identity);
+            currentSpawnCount++;
+        }
+        if (Input.GetAxisRaw("Horizontal") == -1f && CampFire != null)
+        {
+            if (currentSpawnCount >= maxSpawnCount)
+            {
+                Debug.Log("Spawn limit reached.");
+                return;
+            }
+
+            Vector2 spawnPos = spawnPoint ? spawnPoint.position : Vector2.zero;
+            Instantiate(CampFire, spawnPos, Quaternion.identity);
+            currentSpawnCount++;
+        }
+
+
+
+
+
+
     }
+
+    
+    
+
+
+   
+
+
+    
 }
