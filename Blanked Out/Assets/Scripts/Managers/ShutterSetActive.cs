65a13028054e1d385b54ca5389560903f50ea0ad
KLIMenkoXL <taneliklimenko@gmail.com>
2025-05-28 14:32:13 +0300

    Added SoundScript

--- Blanked Out/Assets/Scripts/Managers/ShutterSetActive.cs
+++ Blanked Out/Assets/Scripts/Managers/ShutterSetActive.cs
@@ -1,18 +1,42 @@
+using NUnit.Framework.Constraints;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
+
+
 public class ShutterSetActive : MonoBehaviour
 {
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip OpenDoorSound;
+    public AudioClip CloseDoorSound;
+
+    void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        if(audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+
+            audioSource.Play();
+        }
+
+               
+    }
+
     public bool isOpen = false;
     public void ToggleShutter() 
     {
-        if (gameObject.activeSelf)
+        audioSource.PlayOneShot(OpenDoorSound);
+        if (!isOpen)
         {
+            
             isOpen = true;
             gameObject.SetActive(false);
         }
         else 
         {
+            
             isOpen = false;
             gameObject.SetActive(true);
         }
@@ -20,6 +44,7 @@ public class ShutterSetActive : MonoBehaviour
 
     public void CloseShutter() 
     {
+        audioSource.PlayOneShot(CloseDoorSound);
         gameObject.SetActive(true);    
     }
 
