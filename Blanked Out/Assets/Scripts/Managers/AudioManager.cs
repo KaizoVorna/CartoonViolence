596296e17033bc2ef41a3f4aa62a8837e8da26c2
KLIMenkoXL <taneliklimenko@gmail.com>
2025-06-02 14:48:13 +0300

    The startSound not playing again.

--- Blanked Out/Assets/Scripts/Managers/AudioManager.cs
+++ Blanked Out/Assets/Scripts/Managers/AudioManager.cs
@@ -7,26 +7,39 @@ public class AudioManager : MonoBehaviour
 
 {
 
-    [Header("Audio")]
+    [Header("Audio")]                   // Added a script for game start
     public AudioSource audioSource;
     public AudioClip StartSound;
+    private static bool hasPlayed = false;
 
 
     void Start()
     {
-        audioSource.clip = StartSound;
+        if (!hasPlayed)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = StartSound;
 
-        audioSource.playOnAwake = false;
-        audioSource.volume = 1.0f;
+            audioSource.playOnAwake = false;
 
-        audioSource.PlayOneShot(StartSound);
+            audioSource.volume = 1.0f;
+
+
+            audioSource.PlayOneShot(StartSound);
+            hasPlayed = true;
+        }
+        
+    }
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
     }
 
-  
+
 
     //This is the manager for music, audio cues, etc.
     //To Do:
     //-Create a ground for music changes, situational music?
 
-    
+
 }
