6a75dd55afb5632b9bf2be9ae22c3d34f77b538c
KaizoVorna <KaizoVorna@gmail.com>
2025-06-03 11:48:30 +0300

    Made the respawn into a coroutine
    Thank Tezcatlipoca that it works.

--- Blanked Out/Assets/Scripts/Managers/DeathManager.cs
+++ Blanked Out/Assets/Scripts/Managers/DeathManager.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class DeathManager : MonoBehaviour
 {
     //This script manages how things die.
     //To Do:
     //-Customize each death function once more assets become available. Fall damage can reference PlayerDamage.
+
+    public GameObject deathScreen;
+    public Player_Move movement;
+    public DrawBadManager drawBad;
 
     public void PlayerFallDeath() // This function handles when player falls into pits.
     {
@@ -31,7 +36,32 @@ public class DeathManager : MonoBehaviour
 
     void Reload() 
     {
-        var currentScene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(currentScene.name);
+        var currentScene = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(LoadSceneAsync(currentScene));
+    }
+
+    IEnumerator LoadSceneAsync(int index)
+    {
+        //Activate LoadingScreen
+        if (deathScreen != null)
+            deathScreen.SetActive(true);
+
+        //Start loading the next scene
+        AsyncOperation operation = SceneManager.LoadSceneAsync(index);
+        operation.allowSceneActivation = false;
+
+        //Activate the new scene
+        while (!operation.isDone)
+        {
+            if (operation.progress >= 0.9f)
+            {
+                yield return new WaitForSeconds(2f);
+                operation.allowSceneActivation = true;
+                movement.enabled = true;
+                drawBad.enabled = true;
+            }
+
+            yield return null;
+        }
     }
 }
