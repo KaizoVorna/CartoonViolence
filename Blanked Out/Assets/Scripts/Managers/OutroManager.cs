8336a765d90ffb95d0b313d7244582178db5400c
KaizoVorna <KaizoVorna@gmail.com>
2025-06-04 12:41:34 +0300

    Update OutroManager.cs

--- Blanked Out/Assets/Scripts/Managers/OutroManager.cs
+++ Blanked Out/Assets/Scripts/Managers/OutroManager.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OutroManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject Screen1;
+    public GameObject Screen2;
+    public GameObject Screen3;
+    public GameObject Screen4;
+
+    public void Step1()
     {
-        
+        Screen1.SetActive(false);
+        Screen2.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Step2() 
     {
-        
+        Screen2.SetActive(false);
+        Screen3.SetActive(true);
+    }
+
+    public void Step3()
+    {
+        Screen3.SetActive(false);
+        Screen4.SetActive(true);
+    }
+
+    public void Step4()
+    {
+        Screen4.SetActive(false);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
